8fc668546a8d49de19863f2315e801212622bb23
fhrw <felix.dork@gmail.com>
2023-06-21 23:25:08 +1000

    wrote a test

--- internal/analyze/notes_test.go
+++ internal/analyze/notes_test.go
@@ -1,1 +1,24 @@
 package analyze
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetBarNotes(t *testing.T) {
+	t.Parallel()
+
+	t.Run("get notes in bar", func(t *testing.T) {
+		notes := NoteList{
+			{Track: 1, Pitch: 64, Start: 0, End: 960},
+			{Track: 2, Pitch: 100, Start: 0, End: 960},
+			{Track: 1, Pitch: 100, Start: 960, End: 1920},
+		}
+		want := NoteList{
+			{Track: 1, Pitch: 64, Start: 0, End: 960},
+			{Track: 1, Pitch: 100, Start: 960, End: 1920},
+		}
+		require.Equal(t, want, notes.GetBarNotes(1, 0, 2840))
+	})
+}
